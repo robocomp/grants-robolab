f394d6c719efbf07dc0d98cd736fdffb7e23fced
Daniel Peix del Río <danielpeixdelrio@gmail.com>
2021-03-25 20:36:22 +0100

    gA: 25/03/2021

---
+++ 2021-S1/components/gA/src/CMakeFiles/basecontroller.dir/cmake_clean.cmake
@@ -0,0 +1,39 @@
+file(REMOVE_RECURSE
+  "../bin/basecontroller"
+  "../bin/basecontroller.pdb"
+  "CMakeFiles/basecontroller_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/basecontroller_autogen.dir/ParseCache.txt"
+  "basecontroller_autogen"
+  "CMakeFiles/basecontroller.dir/CommonBehavior.cpp.o"
+  "CMakeFiles/basecontroller.dir/FullPoseEstimation.cpp.o"
+  "CMakeFiles/basecontroller.dir/GenericBase.cpp.o"
+  "CMakeFiles/basecontroller.dir/Laser.cpp.o"
+  "CMakeFiles/basecontroller.dir/OmniRobot.cpp.o"
+  "CMakeFiles/basecontroller.dir/basecontroller_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/basecontroller.dir/commonbehaviorI.cpp.o"
+  "CMakeFiles/basecontroller.dir/genericmonitor.cpp.o"
+  "CMakeFiles/basecontroller.dir/genericworker.cpp.o"
+  "CMakeFiles/basecontroller.dir/main.cpp.o"
+  "CMakeFiles/basecontroller.dir/opt/robocomp/classes/grid2d/grid2d.cpp.o"
+  "CMakeFiles/basecontroller.dir/opt/robocomp/classes/qlog/qlog.cpp.o"
+  "CMakeFiles/basecontroller.dir/opt/robocomp/classes/rapplication/rapplication.cpp.o"
+  "CMakeFiles/basecontroller.dir/opt/robocomp/classes/sigwatch/sigwatch.cpp.o"
+  "CMakeFiles/basecontroller.dir/specificmonitor.cpp.o"
+  "CMakeFiles/basecontroller.dir/specificworker.cpp.o"
+  "CommonBehavior.cpp"
+  "CommonBehavior.h"
+  "FullPoseEstimation.cpp"
+  "FullPoseEstimation.h"
+  "GenericBase.cpp"
+  "GenericBase.h"
+  "Laser.cpp"
+  "Laser.h"
+  "OmniRobot.cpp"
+  "OmniRobot.h"
+  "ui_mainUI.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/basecontroller.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
